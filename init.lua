fd11ffe50937d54a8c00d35840334ae4c7d8d0c3
Corbin <corbinlazarone1@gmail.com>
2025-09-05 23:47:49 -0500

    changed color scheme

--- init.lua
+++ init.lua
@@ -824,7 +824,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'minisummer'
+      vim.cmd.colorscheme 'tokyonight-storm'
     end,
   },
 
