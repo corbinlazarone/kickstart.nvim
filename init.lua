1410c5f6dc4fdeb13ec4318f3aeb4705c28f12e6
Corbin Lazerone <corbinlazarone1@gmail.com>
2025-10-20 15:45:26 -0500

    add c# lsp

--- init.lua
+++ init.lua
@@ -616,6 +616,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        omnisharp = {},
         -- clangd = {},
         html = {
           cmd = { 'vscode-html-language-server', '--stdio' },
